7b2ad462249f96906c478fd699204a279f88aeb0
fchimbera <chimberafreeman@gmail.com>
2025-02-24 00:00:02 +0200

    updated django views and URL configurations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from models import Book, Library
 # list_books view
 def list_books(request):
     books = Book.objects.all()  # Query all books
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # LibraryDetailView class
 class LibraryDetailView(DetailView):
