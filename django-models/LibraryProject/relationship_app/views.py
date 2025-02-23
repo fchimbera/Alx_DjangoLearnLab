bc8c9ac9666bac5d7956bd79e62ae39c0217c36e
fchimbera <chimberafreeman@gmail.com>
2025-02-24 00:04:22 +0200

    updated django views and URL configurations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from models import Book, Library
+from .models import Book
+from .models import Library
 
 # list_books view
 def list_books(request):
@@ -10,5 +11,9 @@ def list_books(request):
 # LibraryDetailView class
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
