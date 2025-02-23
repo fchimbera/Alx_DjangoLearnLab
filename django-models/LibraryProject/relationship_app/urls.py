7cdcb8f22493da635a2c5e84184edbd2d9fb76ec
fchimbera <chimberafreeman@gmail.com>
2025-02-24 00:14:37 +0200

    updated urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from views import LibraryDetailView
+from .views import LibraryDetailView
+from .views import list_books
 from . import views
 
 
