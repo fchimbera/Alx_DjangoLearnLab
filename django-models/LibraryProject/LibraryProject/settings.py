e597641058b6b8a74f9f5ff304012296665f9f71
fchimbera <chimberafreeman@gmail.com>
2025-02-27 06:01:15 +0200

    updated the settings.py template segment

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\CSF\Desktop\alx\Alx_DjangoLearnLab\django-models\LibraryProject\relationship_app\templates'],
+        'DIRS': [r'C:\Users\CSF\Desktop\alx\Alx_DjangoLearnLab\django-models\LibraryProject\relationship_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
