b0a4bd53014d92b5c9bdeedca45600378b52c2fb
fchimbera <chimberafreeman@gmail.com>
2025-03-13 21:29:16 +0200

    updated api/urls.py with url pattern for the view

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import BookList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include the API app's URLs
+    path('books/', BookList.as_view(), name='book-list'),  # Add the BookList view
 ]
