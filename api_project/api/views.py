a2f7258f7f63d93354eb89355d4610ac16a279f7
fchimbera <chimberafreeman@gmail.com>
2025-03-13 21:18:22 +0200

    updated api/urls.py with url pattern for the view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
