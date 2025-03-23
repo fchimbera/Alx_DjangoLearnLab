1302aacd9ed3744a7b39e78266652269449fd270
fchimbera <chimberafreeman@gmail.com>
2025-03-23 19:52:06 +0200

    updated views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,14 +6,14 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from rest_framework.exceptions import ValidationError
 from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import SearchFilter
+from rest_framework import filters
 
 # BookListView is a view that will return a list of all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, filters.OrderingFilter]
 
     # Filterset fields is used to filter the queryset using the filterset_fields attribute
     filterset_fields = ['title', 'author', 'publication_year']
