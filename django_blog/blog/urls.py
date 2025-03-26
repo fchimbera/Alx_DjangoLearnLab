61b597707751479d0927cf7a2de9a935a5894b95
fchimbera <chimberafreeman@gmail.com>
2025-03-27 00:31:39 +0200

    updated blog management features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-]
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-edit'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    ]
