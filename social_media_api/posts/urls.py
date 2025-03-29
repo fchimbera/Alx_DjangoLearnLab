720a11754fb14aef32b1739b4f669e1417783bb1
fchimbera <chimberafreeman@gmail.com>
2025-03-29 21:48:19 +0200

    implemented user follws and feed functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -4,6 +4,6 @@ from .views import PostViewSet, CommentViewSet, FeedViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'feed', FeedViewSet, basename='feed')
+router.register(r'feed/', FeedViewSet, basename='feed')
 
 urlpatterns = router.urls
